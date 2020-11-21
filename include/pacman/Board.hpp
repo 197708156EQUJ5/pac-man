9a55099c17b580e97dbbb435115b756c80561564
197708156EQUJ5 <ggraves73@gmail.com>
2020-11-21 17:41:49 -0500

    Initial checkin Ghost and Pacman

--- include/pacman/Board.hpp
+++ include/pacman/Board.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Pacman.hpp"
+#include "Ghost.hpp"
+
 #include <memory>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -19,8 +22,18 @@ public:
     static const int WIDTH = 672;
     static const int HEIGHT = 864;
 
+    bool checkCollision();
+
 private:
-    
+   
+    uint32_t score;
+    uint32_t lives;
+    std::unique_ptr<Pacman> pacman;
+    std::unique_ptr<Ghost> blinky;
+    std::unique_ptr<Ghost> pinky;
+    std::unique_ptr<Ghost> inky;
+    std::unique_ptr<Ghost> clyde;
+
 };
 
 } // namespace pacman
